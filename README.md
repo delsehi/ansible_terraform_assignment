6d4f6c56a5796e04cc408b5bc6b07568d7959448
Delfi Sehidic <delfi.sehidic@gmail.com>
2021-10-06 17:52:53 +0200

    Update

--- README.md
+++ README.md
@@ -6,7 +6,11 @@ We are Elias, Emil, Lucas and Delfi.
 
 ## Set up 
 Clone this repo. 
+
 Install Terraform. 
+
 Install the Openstack CLI. 
+
 In the dashboard, download the clouds.yaml and add to the directory. 
+
 Create a file terraform.tfvars and add the neccessary information. Look at terraform.tfvars.sample for an example. 
--- terraform/main.tf
+++ terraform/main.tf
@@ -6,8 +6,7 @@ terraform {
   }
 }
 variable "keypair" {
-  type    = string
-  default = "NOT SET" # Set this value in terraform.tfvars
+  type    = string # Set this value in terraform.tfvars
 }
 
 
@@ -31,7 +30,9 @@ resource "openstack_networking_subnet_v2" "subnet_1" {
 
 resource "openstack_networking_router_v2" "public_router" {
     name = "public_router"
-    external_network_id = "fd401e50-9484-4883-9672-a2814089528c"
+    # This must be changed. "openstack network list" to get it. 
+    # TODO: Move to variable
+    external_network_id = "fd401e50-9484-4883-9672-a2814089528c" 
 }
 
 resource "openstack_networking_router_interface_v2" "router_interface" {
@@ -51,17 +52,6 @@ resource "openstack_compute_secgroup_v2" "secgroup_1" {
   }
 }
 
-# resource "openstack_networking_port_v2" "port_1" {
-#   name               = "port_1"
-#   network_id         = openstack_networking_network_v2.network_1.id
-#   admin_state_up     = "true"
-#   security_group_ids = ["${openstack_compute_secgroup_v2.secgroup_1.id}"]
-
-#   fixed_ip {
-#     subnet_id  = openstack_networking_subnet_v2.subnet_1.id
-#     ip_address = "192.168.199.10"
-#   }
-# }
 
 
 # Create an instance
@@ -74,7 +64,6 @@ resource "openstack_compute_instance_v2" "server" {
   security_groups   = ["default", "${openstack_compute_secgroup_v2.secgroup_1.id}"]
 
   network {
-   # port = openstack_networking_port_v2.port_1.id
    name = "network_1"
   }
 }
