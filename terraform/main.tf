e3e886147095e521efd0172913fdf4e01544e279
Delfi Sehidic <delfi.sehidic@gmail.com>
2021-10-06 14:45:44 +0200

    Update readme, adds public ip router and keypair is defined in external file

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,12 +7,9 @@ terraform {
 }
 variable "keypair" {
   type    = string
-  default = "ds222qe_Keypair" # name of keypair created 
+  default = "NOT SET" # Set this value in terraform.tfvars
 }
-variable "network" {
-  type    = string
-  default = "private" # default network to be used
-}
+
 
 variable "security_groups" {
   type    = list(string)
@@ -27,9 +24,19 @@ resource "openstack_networking_network_v2" "network_1" {
 
 resource "openstack_networking_subnet_v2" "subnet_1" {
   name       = "subnet_1"
-  network_id = "${openstack_networking_network_v2.network_1.id}"
+  network_id = openstack_networking_network_v2.network_1.id
   cidr       = "192.168.199.0/24"
   ip_version = 4
+}
+
+resource "openstack_networking_router_v2" "public_router" {
+    name = "public_router"
+    external_network_id = "fd401e50-9484-4883-9672-a2814089528c"
+}
+
+resource "openstack_networking_router_interface_v2" "router_interface" {
+    router_id = openstack_networking_router_v2.public_router.id
+    subnet_id = openstack_networking_subnet_v2.subnet_1.id
 }
 
 resource "openstack_compute_secgroup_v2" "secgroup_1" {
@@ -46,12 +53,12 @@ resource "openstack_compute_secgroup_v2" "secgroup_1" {
 
 resource "openstack_networking_port_v2" "port_1" {
   name               = "port_1"
-  network_id         = "${openstack_networking_network_v2.network_1.id}"
+  network_id         = openstack_networking_network_v2.network_1.id
   admin_state_up     = "true"
   security_group_ids = ["${openstack_compute_secgroup_v2.secgroup_1.id}"]
 
   fixed_ip {
-    subnet_id  = "${openstack_networking_subnet_v2.subnet_1.id}"
+    subnet_id  = openstack_networking_subnet_v2.subnet_1.id
     ip_address = "192.168.199.10"
   }
 }
@@ -59,15 +66,29 @@ resource "openstack_networking_port_v2" "port_1" {
 
 # Create an instance
 resource "openstack_compute_instance_v2" "server" {
-  name            = "server"
-#   image_id        = "1b20a5d7-8ec1-4c2e-af0e-6175d361b680"
-  image_name = "Ubuntu Minimal 18.04"
+  name = "server"
+  image_name        = "Ubuntu Minimal 18.04"
   flavor_name       = "c1-r2-d5"
   availability_zone = "Education"
-  key_pair        = var.keypair
-  security_groups = var.security_groups
+  key_pair          = var.keypair
+  security_groups   = var.security_groups
 
   network {
-    port = "${openstack_networking_port_v2.port_1.id}"
+   # port = openstack_networking_port_v2.port_1.id
+   name = "network_1"
   }
+}
+
+resource "openstack_networking_floatingip_v2" "fip_1" {
+  pool = "public"
+}
+
+resource "openstack_compute_floatingip_associate_v2" "fip_1" {
+  floating_ip = "${openstack_networking_floatingip_v2.fip_1.address}"
+  instance_id = "${openstack_compute_instance_v2.server.id}"
+}
+
+output "public_ip" {
+    value = openstack_networking_floatingip_v2.fip_1.address
+    description = "The public ip address of the server"
 }
