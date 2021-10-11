c357f46c9c431fa95b8d2555530f4159846e8149
Elias Wändal <elias.wandal@hotmail.com>
2021-10-11 09:15:06 +0200

    fix db_slave instance name

--- terraform/db.tf
+++ terraform/db.tf
@@ -14,7 +14,7 @@ resource "openstack_compute_instance_v2" "db_master" {
 
 # Create database slave node
 resource "openstack_compute_instance_v2" "db_slave" {
-  name              = "db_master"
+  name              = "db_slave"
   image_name        = "Ubuntu server 20.04"
   flavor_name       = "c1-r2-d5"
   availability_zone = "Education"
