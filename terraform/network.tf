e2d939212f1786c6f670529a538ac2af4a48bba4
Emil Solin <ef222ut@student.lnu.se>
2021-10-18 12:57:09 +0200

    fip_1 depending on control node

--- terraform/network.tf
+++ terraform/network.tf
@@ -131,4 +131,8 @@ resource "openstack_networking_floatingip_v2" "loadbalancer_fip" {
 resource "openstack_networking_floatingip_associate_v2" "fip_1" {
   floating_ip = openstack_networking_floatingip_v2.control_node_fip.address
   port_id     = openstack_networking_port_v2.control_node_port.id
+
+  depends_on = [
+    openstack_compute_instance_v2.control_node
+  ]
 }
