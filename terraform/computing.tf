3b62934108cdc62dd1b803ac99392e72cc0eb708
Emil Solin <ef222ut@student.lnu.se>
2021-10-26 10:26:15 +0200

    Adding depends on

--- terraform/computing.tf
+++ terraform/computing.tf
@@ -6,6 +6,11 @@ module "control_node" {
   port_id   = module.control_node_port.id
   keypair   = var.keypair
   user_data = templatefile("./templates/cn_config.tpl", local.install_ansible)
+
+  depends_on = [
+    module.network.router_interface,
+    module.control_node_floating_ip
+  ]
 }
 
 
@@ -18,6 +23,9 @@ module "wordpress" {
   port_id = module.wordpress_port[count.index].id
   keypair = var.keypair
 
+  depends_on = [
+    module.network.router_interface
+  ]
 }
 
 module "database" {
@@ -27,6 +35,10 @@ module "database" {
   name    = "db_${count.index + 1}"
   port_id = module.database_port[count.index].id
   keypair = var.keypair
+
+  depends_on = [
+    module.network.router_interface
+  ]
 }
 
 module "fileserver" {
@@ -35,4 +47,8 @@ module "fileserver" {
   name    = "fileserver"
   port_id = module.fileserver_port.id
   keypair = var.keypair
+
+  depends_on = [
+    module.network.router_interface
+  ]
 }
