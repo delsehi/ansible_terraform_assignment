4154f00ad5d3307e28f3f097eda5f6439cc06bc0
Emil Solin <ef222ut@student.lnu.se>
2021-10-26 10:25:09 +0200

    Outputting wordpress instances, control node ip

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,11 @@
-output "wordpress" {
-  value = module.wordpress[*].internal_ip
+output "wordpress_instances" {
+  value = length(module.wordpress[*])
 }
 
 output "loadbalancer_ip" {
   value = module.loadbalancer_floating_ip.address
+}
+
+output "control_node_ip" {
+  value = module.control_node_floating_ip.address
 }
