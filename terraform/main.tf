07a739a4379e8ffb43a4843163b1a03df60cb733
Emil Solin <ef222ut@student.lnu.se>
2021-10-26 10:40:40 +0200

    Moving loadbalancer instantiation to computing, again

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,3 @@
-module "loadbalancer" {
-  source = "./modules/loadbalancer"
 
-  name         = "loadbalancer"
-  subnet_id    = module.network.subnet_id
-  secgroup_ids = [module.ssh_secgroup.id, module.http_secgroup.id]
-  members      = module.wordpress[*].internal_ip
-}
 
 
