8c2b9bbaf9a683ba8b61f0e905784c58fb9b8e5c
Emil Solin <ef222ut@student.lnu.se>
2021-10-26 10:24:27 +0200

    Exporting router_interface

--- terraform/modules/network/outputs.tf
+++ terraform/modules/network/outputs.tf
@@ -6,6 +6,10 @@ output "subnet_id" {
   value = openstack_networking_subnet_v2.subnet.id
 }
 
+output "router_interface" {
+  value = openstack_networking_router_interface_v2.router_interface
+}
+
 output "router_id" {
   value = openstack_networking_router_v2.router.id
 }
