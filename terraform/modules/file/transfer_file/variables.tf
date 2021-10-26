4a8315c8ddf7beffb81fc3765040879afa60feaf
Emil Solin <ef222ut@student.lnu.se>
2021-10-26 10:19:38 +0200

    Adding transfer_file module

---
+++ terraform/modules/file/transfer_file/variables.tf
@@ -0,0 +1,18 @@
+variable "source_file" {
+
+}
+variable "destination_file" {
+
+}
+variable "remote_host" {
+
+}
+variable "connection_type" {
+  default = "ssh"
+}
+variable "user" {
+  default = "ubuntu"
+}
+variable "private_key" {
+
+}
